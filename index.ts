13e9ca06e9cc5a5e7bd17b45bdd644a8893239da
yang-danny <yang_danny@hotmail.com>
2025-10-27 15:28:17 +1100

    run server and client together

---
+++ index.ts
@@ -0,0 +1,15 @@
+import concurrently from 'concurrently';
+concurrently([
+    {
+        name: 'server',
+        command: 'npm run dev',
+        cwd: 'packages/server',
+        prefixColor: 'blue'
+    },
+      {
+        name: 'client',
+        command: 'npm run dev',
+        cwd: 'packages/client',
+        prefixColor: 'green'
+    }
+])
